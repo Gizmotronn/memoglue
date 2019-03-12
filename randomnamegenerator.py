6452cb029aab13c0026d55ce1ba5f60ab90d0437
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-03-12 11:36:27 +0800

    added what's your name

--- randomnamegenerator.py
+++ randomnamegenerator.py
@@ -1,2 +1,6 @@
 import time
 print("Hello, and welcome back. I hope you enjoyed the previous mini-app I created for DroidOS Python")
+time.sleep(2)
+fh = input("What is your current name?")
+time.sleep(1)
+print("Hello, " + fh)
