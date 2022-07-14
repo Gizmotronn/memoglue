91720b21018c40369d16da7b00993f5926ceab94
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-14 14:01:14 +0800

    🧊🍭 -> Add swiftview ui to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import NotesList from "./components/NotesList";
+import './App.css';
 
 const App = () => {
-  return <div className="container">
-    <NotesList />
-  </div>
+  return (
+    <div className="container">
+      <NotesList />
+    </div>
+  );
 };
 
 export default App;
